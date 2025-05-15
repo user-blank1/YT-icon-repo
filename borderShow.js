732b720adbaadb1f72cae0a1887533aa92e99251
user-blank1 <linaszilinskasas@gmail.com>
2025-05-15 16:22:11 +0300

    all buttons done, changing buttons need fixing

--- borderShow.js
+++
@@ -1,4 +0,0 @@
-chrome.storage.onChanged.addListener((changes, areaName) => {
-    if (areaName === "local" && changes.chosenIcons) {
-    }
-});
--- youtubeEdit.js
+++ youtubeEdit.js
@@ -416,6 +416,124 @@ const applyMiniplayerImg = (buttonEl, id, base64data, title) => {
     buttonEl.style.position = "relative";
     buttonEl.appendChild(img);
 };
+const applyTheater = (buttonEl, id, iconClass, title) => {
+    if (!buttonEl) return;
+    const currentIcon = buttonEl.querySelector("i");
+    const svgIcon = buttonEl.querySelector("svg");
+
+    if (svgIcon) svgIcon.style.visibility = "hidden"; // Hide the SVG icon
+
+    // If an icon already exists, update it
+    if (currentIcon) {
+        currentIcon.className = `bi bi-${iconClass}`;
+        currentIcon.title = title;
+    } else {
+        const icon = document.createElement("i");
+        icon.className = `bi bi-${iconClass}`;
+        icon.id = id;
+        icon.title = title;
+        icon.style.cssText = `
+            color: white !important;
+            fill: white !important;
+            z-index: 9999;
+            position: absolute;
+            pointer-events: none;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            font-size: 18px;
+        `;
+
+        buttonEl.style.position = "relative";
+        buttonEl.appendChild(icon);
+    }
+};
+const applyTheaterImg = (buttonEl, id, base64data, title) => {
+    if (!buttonEl) return;
+
+    const svgIcon = buttonEl.querySelector("svg");
+    const currentIcon = buttonEl.querySelector("i");
+
+    if (svgIcon) svgIcon.style.visibility = "hidden";
+    if (currentIcon) currentIcon.remove();
+
+    const img = document.createElement("img");
+    img.src = base64data;
+    img.id = id;
+    img.title = title;
+    img.style.cssText = `
+        z-index: 9999;
+        position: absolute;
+        pointer-events: none;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        width: 35px;
+        height: 35px;
+    `;
+
+    buttonEl.style.position = "relative";
+    buttonEl.appendChild(img);
+};
+const applyFullScreen = (buttonEl, id, iconClass, title) => {
+    if (!buttonEl) return;
+    const currentIcon = buttonEl.querySelector("i");
+    const svgIcon = buttonEl.querySelector("svg");
+
+    if (svgIcon) svgIcon.style.visibility = "hidden"; // Hide the SVG icon
+
+    // If an icon already exists, update it
+    if (currentIcon) {
+        currentIcon.className = `bi bi-${iconClass}`;
+        currentIcon.title = title;
+    } else {
+        const icon = document.createElement("i");
+        icon.className = `bi bi-${iconClass}`;
+        icon.id = id;
+        icon.title = title;
+        icon.style.cssText = `
+            color: white !important;
+            fill: white !important;
+            z-index: 9999;
+            position: absolute;
+            pointer-events: none;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            font-size: 18px;
+        `;
+
+        buttonEl.style.position = "relative";
+        buttonEl.appendChild(icon);
+    }
+};
+const applyFullScreenImg = (buttonEl, id, base64data, title) => {
+    if (!buttonEl) return;
+
+    const svgIcon = buttonEl.querySelector("svg");
+    const currentIcon = buttonEl.querySelector("i");
+
+    if (svgIcon) svgIcon.style.visibility = "hidden";
+    if (currentIcon) currentIcon.remove();
+
+    const img = document.createElement("img");
+    img.src = base64data;
+    img.id = id;
+    img.title = title;
+    img.style.cssText = `
+        z-index: 9999;
+        position: absolute;
+        pointer-events: none;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        width: 35px;
+        height: 35px;
+    `;
+
+    buttonEl.style.position = "relative";
+    buttonEl.appendChild(img);
+};
 // LISTEN TO CHANGES IN STORAGE
 chrome.storage.onChanged.addListener((changes, areaName) => {
     if (areaName === "local" && changes.chosenIcons) {
@@ -432,6 +550,8 @@ chrome.storage.onChanged.addListener((changes, areaName) => {
         const cc = document.querySelector(".ytp-subtitles-button");
         const settings = document.querySelector(".ytp-settings-button");
         const miniplayer = document.querySelector(".ytp-miniplayer-button");
+        const theater = document.querySelector(".ytp-size-button");
+        const fullscreen = document.querySelector(".ytp-fullscreen-button");
         icons.forEach((icon) => {
             if (icon.content === "ICON") {
                 if (icon.name[0] === "play") applyPlay(play, "play2", icon.data, icon.data);
@@ -441,6 +561,8 @@ chrome.storage.onChanged.addListener((changes, areaName) => {
                 else if (icon.name[0] === "settings") applySettings(settings, "settings", icon.data, icon.data);
                 else if (icon.name[0] === "cc") applyCC(cc, "cc", icon.data, icon.data);
                 else if (icon.name[0] === "pip") applyMiniplayer(miniplayer, "miniplayer", icon.data, icon.data);
+                else if (icon.name[0] === "theater") applyTheater(theater, "theater", icon.data, icon.data);
+                else if (icon.name[0] === "fullscreen") applyFullScreen(fullscreen, "fullscreen", icon.data, icon.data);
             } else if (icon.content === "IMAGE") {
                 if (icon.name[0] === "play") applyPlayImg(play, "play2", icon.data, icon.data);
                 else if (icon.name[0] === "skip") applySkipImg(skip, "skip", icon.data, icon.data);
@@ -449,6 +571,8 @@ chrome.storage.onChanged.addListener((changes, areaName) => {
                 else if (icon.name[0] === "cc") applyCCImg(cc, "cc", icon.data, icon.data);
                 else if (icon.name[0] === "settings") applySettingsImg(settings, "settings", icon.data, icon.data);
                 else if (icon.name[0] === "pip") applyMiniplayerImg(miniplayer, "miniplayer", icon.data, icon.data);
+                else if (icon.name[0] === "theater") applyTheaterImg(theater, "theater", icon.data, icon.data);
+                else if (icon.name[0] === "fullscreen") applyFullScreenImg(fullscreen, "fullscreen", icon.data, icon.data);
             }
         });
     }
@@ -470,6 +594,7 @@ function applySavedIcons() {
         const volume = document.querySelector(".ytp-volume-icon");
         const autoplay = document.querySelector(".ytp-autonav-toggle");
         const cc = document.querySelector(".ytp-subtitles-button");
+        const theater = document.querySelector(".ytp-size-button");
         icons.forEach((icon) => {
             if (icon.content === "ICON") {
                 if (icon.name[0] === "play") applyPlay(play, "play2", icon.data, icon.data);
@@ -479,6 +604,8 @@ function applySavedIcons() {
                 else if (icon.name[0] === "cc") applyCC(cc, "cc", icon.data, icon.data);
                 else if (icon.name[0] === "pip") applyMiniplayer(miniplayer, "miniplayer", icon.data, icon.data);
                 else if (icon.name[0] === "settings") applySettings(settings, "settings", icon.data, icon.data);
+                else if (icon.name[0] === "theater") applyTheater(theater, "theater", icon.data, icon.data);
+                else if (icon.name[0] === "fullscreen") applyFullScreen(fullscreen, "fullscreen", icon.data, icon.data);
             } else if (icon.content === "IMAGE") {
                 if (icon.name[0] === "play") applyPlayImg(play, "play2", icon.data, icon.data);
                 else if (icon.name[0] === "skip") applySkipImg(skip, "skip", icon.data, icon.data);
@@ -487,6 +614,8 @@ function applySavedIcons() {
                 else if (icon.name[0] === "cc") applyCCImg(cc, "cc", icon.data, icon.data);
                 else if (icon.name[0] === "pip") applyMiniplayerImg(miniplayer, "miniplayer", icon.data, icon.data);
                 else if (icon.name[0] === "settings") applySettingsImg(settings, "settings", icon.data, icon.data);
+                else if (icon.name[0] === "theater") applyTheaterImg(theater, "theater", icon.data, icon.data);
+                else if (icon.name[0] === "fullscreen") applyFullScreenImg(fullscreen, "fullscreen", icon.data, icon.data);
             }
         });
     });
@@ -585,7 +714,9 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
         const cc = document.querySelector(".ytp-subtitles-button");
         const settings = document.querySelector(".ytp-settings-button");
         const miniplayer = document.querySelector(".ytp-miniplayer-button");
-        [play, skip, volume, autoplay, cc, settings, miniplayer].forEach((btn) => {
+        const theater = document.querySelector(".ytp-size-button");
+        const fullscreen = document.querySelector(".ytp-fullscreen-button");
+        [play, skip, volume, autoplay, cc, settings, miniplayer, theater, fullscreen].forEach((btn) => {
             if (!btn) return;
             const customIcon = btn.querySelector("i, img");
             const svgIcon = btn.querySelector("svg");
@@ -604,6 +735,8 @@ function clearIconsFromDOM() {
         document.querySelector(".ytp-subtitles-button"),
         document.querySelector(".ytp-settings-button"),
         document.querySelector(".ytp-miniplayer-button"),
+        document.querySelector(".ytp-size-button"),
+        document.querySelector(".ytp-fullscreen-button"),
     ];
     buttons.forEach((btn) => {
         if (!btn) return;
@@ -616,7 +749,7 @@ function clearIconsFromDOM() {
 function clearIconsFromDOM2() {
     document
         .querySelectorAll(
-            ".ytp-miniplayer-button i, .ytp-play-button i, .ytp-settings-button i,.ytp-subtitles-button i, .ytp-next-button i, .ytp-volume-icon i, .ytp-autonav-toggle i, .ytp-play-button img, .ytp-next-button img, .ytp-volume-icon img, .ytp-autonav-toggle img,"
+            ".ytp-fullscreen-button i,.ytp-size-button i,.ytp-miniplayer-button i, .ytp-play-button i, .ytp-settings-button i,.ytp-subtitles-button i, .ytp-next-button i, .ytp-volume-icon i, .ytp-autonav-toggle i, .ytp-play-button img, .ytp-next-button img, .ytp-volume-icon img, .ytp-autonav-toggle img,"
         )
         .forEach((el) => el.remove());
 
@@ -631,4 +764,7 @@ function clearIconsFromDOM2() {
     resetVisibility(".ytp-autonav-toggle");
     resetVisibility(".ytp-subtitles-button");
     resetVisibility(".ytp-settings-button");
+    resetVisibility(".ytp-miniplayer-button");
+    resetVisibility(".ytp-size-button");
+    resetVisibility(".ytp-fullscreen-button");
 }
